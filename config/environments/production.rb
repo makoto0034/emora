6ddc7b00d27193e44c45e99b56aef4f580e8f139
MAKOTO SAITO <makotosaitou@me.com>
2019-09-21 16:31:06 +0900

    mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'emora.herokuapp.com', port: 443 }
+  config.action_mailer.default_url_options = { host: 'emora.herokuapp.com', port: 80 }
 
   config.action_mailer.delivery_method = :smtp
 
