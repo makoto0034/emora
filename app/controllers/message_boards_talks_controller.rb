267e45834ac92f9a7cb78bbf2822b969172371f8
MAKOTO SAITO <makotosaitou@me.com>
2019-10-23 17:22:33 +0900

    desigin at secoud tme

--- app/controllers/message_boards_talks_controller.rb
+++ app/controllers/message_boards_talks_controller.rb
@@ -1,5 +1,6 @@
 class MessageBoardsTalksController < ApplicationController
   def index
     @posts=Post.where(message_board_id:params[:id])
+    @message_board = MessageBoard.find(params[:id])
   end
 end
