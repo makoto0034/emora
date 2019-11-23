4f56a606aefa7c02639da029faecb746976ffbf7
MAKOTO SAITO <makotosaitou@me.com>
2019-11-23 11:48:07 +0900

    user_show redevised

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @posts=Post.where(user_id:current_user)
+    @posts=Post.where(user_id:params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
