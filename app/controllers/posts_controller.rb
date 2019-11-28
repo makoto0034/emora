337c3ad2f02593e10015853d0676b16f97229b06
MAKOTO SAITO <makotosaitou@me.com>
2019-11-28 22:58:39 +0900

    delete flaah

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
     @post=Post.new(content:params[:content],message_board_id:params[:id],user_id:current_user.id)
 
       if @post.save
-        flash[:success] = '投稿しました'
         redirect_to(message_boards_talks_path(params[:id],category_id:params[:category_id]))
       else
         flash[:alert] = '空文字は投稿できません'
