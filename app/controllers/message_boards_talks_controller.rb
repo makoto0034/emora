681f25be201069a59353e23c2842dcad12e77a16
MAKOTO SAITO <makotosaitou@me.com>
2019-12-12 01:54:48 +0900

    message_board index redesigned

--- app/controllers/message_boards_talks_controller.rb
+++ app/controllers/message_boards_talks_controller.rb
@@ -1,6 +1,6 @@
 class MessageBoardsTalksController < ApplicationController
   def index
-    @posts=Post.where(message_board_id:params[:id])
+    @posts=Post.where(message_board_id:params[:id]).order(created_at: :desc)
     @message_board = MessageBoard.find(params[:id])
     @message_boards=MessageBoard.where(category_id:params[:category_id])
   end
