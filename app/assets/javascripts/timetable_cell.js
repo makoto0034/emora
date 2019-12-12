8530fe2a643e974fe542182c7ef24815d119dccf
MAKOTO SAITO <makotosaitou@me.com>
2019-12-12 15:32:38 +0900

    message__board_talks index redesigned

--- app/assets/javascripts/timetable_cell.js
+++ app/assets/javascripts/timetable_cell.js
@@ -1,17 +1,17 @@
-$(".timetable_blank").click(function(){
-  day_of_the_week_id = ($(this).data('week'));
-  period_id = ($(this).data('period'));
-
-  console.log(day_of_the_week_id);
-  console.log(period_id);
-
-  $.ajax({
-    url:'/timetable_cells/:id/timetable_cell_contents/new',
-    type:"GET",
-    data:{
-      day_of_the_week_id:day_of_the_week_id,
-      period_id:period_id
-    }
-  });
-
-});
+// $(".timetable_blank").click(function(){
+//   day_of_the_week_id = ($(this).data('week'));
+//   period_id = ($(this).data('period'));
+//
+//   console.log(day_of_the_week_id);
+//   console.log(period_id);
+//
+//   $.ajax({
+//     url:'/timetable_cells/:id/timetable_cell_contents/new',
+//     type:"GET",
+//     data:{
+//       day_of_the_week_id:day_of_the_week_id,
+//       period_id:period_id
+//     }
+//   });
+//
+// });
