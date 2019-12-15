9978a86b1fc6b154238f59237fb49180b7b3dd32
MAKOTO SAITO <makotosaitou@me.com>
2019-12-15 18:47:56 +0900

    hamburdermenu redesigned

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,16 @@ $(document).on('turbolinks:load', function(){
   setTimeout("$('.alert').fadeOut('slow')", 3000)
   setTimeout("$('.success').fadeOut('slow')", 3000)
 });
+
+
+$(document).on('turbolinks:load', function(){
+    var $body = document.body
+    , $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
+
+    if ( typeof $menu_trigger !== 'undefined' ) {
+        $menu_trigger.addEventListener('click', function() {
+            $body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
+        });
+    }
+
+}).call(this);
